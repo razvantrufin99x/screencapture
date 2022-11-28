1860ec123a06d55e7c974f8d487f6711582cde27
RAZVAN TRUFIN <razvantrufin99x@gmail.com>
2022-11-28 20:42:48 +0200

    Add project files.

---
+++ screencapture/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace screencapture
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Bitmap screen = new Bitmap(Screen.PrimaryScreen.Bounds.Width,
+Screen.PrimaryScreen.Bounds.Height);
+            using (Graphics g = Graphics.FromImage(screen))
+            {
+                g.CopyFromScreen(0, 0, 0, 0, screen.Size);
+            }
+            pictureBox1.BackgroundImage = screen;
+            pictureBox1.Image = screen;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+       
+    }
+}
